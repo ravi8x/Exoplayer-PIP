c90ea2b4bc61ed638cd62f95caac1a2dd01a71b1
Ravi Tamada <ravi8x@gmail.com>
2024-10-17 18:39:58 +0530

    Example of playing exoplayer video in PIP mode

---
+++ app/src/main/java/info/androidhive/exoplayerpip/MainActivity.kt
@@ -0,0 +1,41 @@
+package info.androidhive.exoplayerpip
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import info.androidhive.exoplayerpip.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val title = "Big Buck Bunny"
+    private var mediaUrlLandscape: String =
+        "https://firebasestorage.googleapis.com/v0/b/project-8525323942962534560.appspot.com/o/samples%2FBig%20Buck%20Bunny%2060fps%204K%20-%20Official%20Blender%20Foundation%20Short%20Film.mp4?alt=media&token=351ab76e-6e1f-43eb-b868-0a060277a338"
+
+    private var mediaUrlPortrait: String =
+        "https://firebasestorage.googleapis.com/v0/b/project-8525323942962534560.appspot.com/o/samples%2F15365448-hd_1080_1920_30fps.mp4?alt=media&token=4e2bc0e5-42f9-412c-9681-df20aa00599d"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.content.btnVideoLandscape.setOnClickListener {
+            playVideo(mediaUrlLandscape)
+        }
+
+        binding.content.btnVideoPortrait.setOnClickListener {
+            playVideo(mediaUrlPortrait, true)
+        }
+    }
+
+    private fun playVideo(url: String, isPortrait: Boolean = false) {
+        startActivity(Intent(this, PlayerActivity::class.java).apply {
+            putExtra("url", url)
+            putExtra("is_portrait", isPortrait)
+        })
+    }
+}
---
+++ app/src/main/java/info/androidhive/exoplayerpip/PlayerActivity.kt
@@ -0,0 +1,265 @@
+package info.androidhive.exoplayerpip
+
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.graphics.Rect
+import android.graphics.drawable.Icon
+import android.os.Build
+import android.os.Bundle
+import android.util.Rational
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.DrawableRes
+import androidx.annotation.OptIn
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import info.androidhive.exoplayerpip.databinding.ActivityPlayerBinding
+
+
+class PlayerActivity : AppCompatActivity() {
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityPlayerBinding.inflate(layoutInflater)
+    }
+    private var player: Player? = null
+    private var mediaUrl: String? = null
+    private var isPortrait: Boolean = false
+
+    private val isPipSupported by lazy {
+        packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+    }
+
+    companion object {
+        private const val ACTION_PLAYER_CONTROLS = "action_player_controls"
+        private const val EXTRA_CONTROL_TYPE = "control_type"
+        private const val CONTROL_PLAY_PAUSE = 1
+        private const val REQUEST_PLAY_OR_PAUSE = 2
+    }
+
+    private var callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            handleBackPressed()
+        }
+    }
+
+    // broadcast receiver to receiver actions when pip window action buttons are tapped
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent == null || intent.action != ACTION_PLAYER_CONTROLS) {
+                return
+            }
+            when (intent.getIntExtra(EXTRA_CONTROL_TYPE, 0)) {
+                // Toggle b/w play and pause
+                CONTROL_PLAY_PAUSE -> {
+                    if (binding.playerView.player?.isPlaying == true) {
+                        binding.playerView.player?.pause()
+                    } else {
+                        binding.playerView.player?.play()
+                    }
+
+                    // update the pip window actions after player is paused or resumed
+                    updatePictureInPictureParams()
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        mediaUrl = intent.extras?.getString("url")
+        isPortrait = intent.extras?.getBoolean("is_portrait") ?: false
+
+        if (mediaUrl.isNullOrBlank()) {
+            Toast.makeText(this, "Media url is null!", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        // register the broadcast receiver
+        registerReceiver(broadcastReceiver, IntentFilter(ACTION_PLAYER_CONTROLS))
+    }
+
+    @OptIn(UnstableApi::class)
+    private fun initializePlayer() {
+        // release the player if the activity is already running in PIP mode
+        if (player != null) {
+            releasePlayer()
+        }
+
+        player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            binding.playerView.player = exoPlayer
+
+            val mediaBuilder = MediaItem.Builder().setUri(mediaUrl)
+            val mediaItem = mediaBuilder.build()
+            exoPlayer.setMediaItems(listOf(mediaItem))
+            exoPlayer.playWhenReady = true
+            exoPlayer.prepare()
+        }
+    }
+
+    /**
+     * Enter into PIP when back is pressed and video is playing
+     * */
+    fun handleBackPressed() {
+        if (!isPipSupported) {
+            finish()
+            return
+        }
+
+        if (!isInPictureInPictureMode && binding.playerView.player?.isPlaying == true) {
+            enterPipMode()
+        } else {
+            // video is not playing, finish the activity
+            finish()
+        }
+    }
+
+    // enter into PIP when home is pressed
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        enterPipMode()
+    }
+
+    private fun enterPipMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            enterPictureInPictureMode(updatePictureInPictureParams())
+        } else {
+            enterPictureInPictureMode()
+        }
+    }
+
+    // Updating picture in picture param
+    private fun updatePictureInPictureParams(): PictureInPictureParams {
+        val visibleRect = Rect()
+        binding.playerView.getGlobalVisibleRect(visibleRect)
+
+        // Aspect ratio based on video size landscape or portrait
+        val rational = if (isPortrait) {
+            Rational(9, 16)
+        } else {
+            Rational(16, 9)
+        }
+
+        val builder = PictureInPictureParams.Builder()
+            .setAspectRatio(rational)
+            .setActions(
+                listOf(
+                    // Keeping play or pause action based on player state
+                    if (binding.playerView.player?.isPlaying == false) {
+                        // video is not playing. Keep play action
+                        createRemoteAction(
+                            R.drawable.ic_play,
+                            R.string.play,
+                            REQUEST_PLAY_OR_PAUSE,
+                            CONTROL_PLAY_PAUSE
+                        )
+                    } else {
+                        // video is playing. Keep pause action
+                        createRemoteAction(
+                            R.drawable.ic_pause,
+                            R.string.pause,
+                            REQUEST_PLAY_OR_PAUSE,
+                            CONTROL_PLAY_PAUSE
+                        )
+                    }
+                )
+            )
+            .setSourceRectHint(visibleRect)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // enable auto enter on Android 12
+            builder.setAutoEnterEnabled(true)
+        }
+
+        val params = builder.build()
+        setPictureInPictureParams(params)
+
+        return params
+    }
+
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+
+        // Hide player controls when in PIP mode
+        binding.playerView.useController = !isInPictureInPictureMode
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        // receiving new media url when activity is running in PIP mode
+        mediaUrl = intent.extras?.getString("url")
+        isPortrait = intent.extras?.getBoolean("is_portrait") ?: false
+        initializePlayer()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        callback.remove()
+        onBackPressedDispatcher.addCallback(this, callback)
+        initializePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (!isInPictureInPictureMode) {
+            binding.playerView.onPause()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.playerView.useController = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        callback.remove()
+        releasePlayer()
+
+        // remove the activity from recent tasks as PIP activity won't be
+        // removed automatically
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)) {
+            finishAndRemoveTask()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.release()
+        player = null
+    }
+
+    // Method to create action button for pip window
+    private fun createRemoteAction(
+        @DrawableRes iconResId: Int,
+        @StringRes titleResId: Int,
+        requestCode: Int,
+        controlType: Int
+    ): RemoteAction {
+        return RemoteAction(
+            Icon.createWithResource(this, iconResId),
+            getString(titleResId),
+            getString(titleResId),
+            PendingIntent.getBroadcast(
+                this,
+                requestCode,
+                Intent(ACTION_PLAYER_CONTROLS).putExtra(EXTRA_CONTROL_TYPE, controlType),
+                PendingIntent.FLAG_IMMUTABLE
+            )
+        )
+    }
+}
